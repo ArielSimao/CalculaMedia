1248519f9d99de17dd3027d2ccae7ff8b8f6ecc2
ArielSimao <arielsimao@outlook.com>
2018-03-06 15:22:27 -0300

    Initial commit

---
+++ CalculaMedia/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculaMedia
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void btnSair_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult resposta = MessageBox.Show("Voce Deseja Fechar Aplicação ? ", "Sair",
+                MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (resposta == MessageBoxResult.Yes) {
+                Close();
+                    } 
+        }
+
+       
+
+        private void btnLimpar_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnCalcularMedia_Click(object sender, RoutedEventArgs e)
+        {
+            double N1, N2, Media;
+            N1 = Convert.ToDouble(txtNota1.Text);
+            N2 = Convert.ToDouble(txtNota2.Text);
+
+            Media = (N1 + N2 / 2);
+
+            txtMedia.Text = Convert.ToString(Media);
+
+            if (Media>= 7)
+            {
+                MessageBox.Show("Aluno Aprovado", "Resultado",MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    else
+                MessageBox.Show("Aluno Aprovado", "Resultado",MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+        }
+    }
+}
